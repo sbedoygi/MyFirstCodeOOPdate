c16fb25297acbb0cacbc8305cb26cefd958bc8fb
sebas <sebas@DESKTOP-KGBLMEK>
2023-02-24 10:16:47 -0500

    Add project files.

---
+++ MyFirstCodeOOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstCodeOOP
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
